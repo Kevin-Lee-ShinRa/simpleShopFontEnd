850f34c604944cd31a423a42e7420a8f3db42c61
Kevin-Lee-ShinRa <1799674116@qq.com>
2024-10-05 18:22:51 +0800

    update code

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import router from '@/app/pkg/router';
 import store from '@/app/pkg/store';
-import SrComponents from '@/app/sr-vue-components';
+// import SrComponents from '@/app/sr-vue-components';
 import SrTextUpTransition from '@/app/pkg/transition/sr-text-up-transition.vue';
 import SrTextDownTransition from '@/app/pkg/transition/sr-text-down-transition.vue';
 
@@ -13,9 +13,9 @@ Vue.prototype.bus = new Vue();
 Vue.prototype.$appBarHeight = 60;
 Vue.prototype.$globalOverlay = 0.5;
 
-Vue.use(SrComponents);
-Vue.component('sr-text-up-transition', SrTextUpTransition);
-Vue.component('sr-text-down-transition', SrTextDownTransition);
+// Vue.use(SrComponents);
+// Vue.component('sr-text-up-transition', SrTextUpTransition);
+// Vue.component('sr-text-down-transition', SrTextDownTransition);
 
 new Vue({
   router,
